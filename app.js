ab9d1308941178a2533ff0497010a675e031819e
00010265 <wiut00010265@gmail.com>
2021-04-06 14:51:38 +0500

    fronted changes

--- app.js
+++ app.js
@@ -4,14 +4,32 @@ const PORT = 8000
 
 app.set('view engine', 'pug')
 
-app.use('/static', express.static('public'))
+app.use('/static', express.static('public'))//assets
+app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req,res) => {
-    res.render('index')
-})
-
+app.get('/', (req, res) => {
+    const blogs = [
+      {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+      {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    ];
+    res.render('index', { title: 'Home', blogs });
+  });
+  
+  app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+  });
+  
+  app.get('/blogs/create', (req, res) => {
+    res.render('create', { title: 'New data' });
+  });
+  
+  // 404 page
+  app.use((req, res) => {
+    res.status(404).render('404', { title: '404' });
+  });
 app.listen(PORT, (err) => {
     if (err) throw err
 
-    console.log('This app is running on port ${PORT}')
+    console.log('This app is running on port 8000')
 })
